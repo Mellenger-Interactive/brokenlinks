20a37d728a652ee1005a214d4652203f4f5cff41
Scott Falkingham <scott@falkingham.ca>
2024-12-19 14:48:48 -0600

    Create CSV file.

--- brokenlinkscsv.js
+++ brokenlinkscsv.js
@@ -1,7 +1,7 @@
 var lines = document.querySelectorAll('#brokenLinkTable tr:not(:first-child)');
-
 var line_string = '﻿#,Problematic link,Server response,Page containing the link,Source viewer\n';
 var i = 1;
+var link = document.createElement('a');
 lines.forEach((line) => {
   line_string += i++ + ',';
   var url = line.querySelector('td:nth-child(2) input');
@@ -14,4 +14,6 @@ lines.forEach((line) => {
   line_string += source.getAttribute("href");
   line_string += '\n';
 });
-console.log(line_string);
+link.href = URL.createObjectURL(new Blob([line_string], { type: 'text/csv' }));
+link.download = 'Broken Links - ' + document.querySelector('form#inputForm input[name="url_input"]').value + ' - ' + new Date().toISOString().slice(0, 10) + '.csv';
+link.click();
